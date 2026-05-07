ce865f5e280ea40993f65b0235d3ad6917669af1
shiki1205 <1513632469@qq.com>
2026-05-07 16:46:32 +0800

    [feat]:完成室内地图绘制

--- Assets/Scripts/TilemapScripts/SwitchScene.cs
+++ Assets/Scripts/TilemapScripts/SwitchScene.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
 
-
 public class SwitchScene : MonoBehaviour
 {
 
-    public string sceneName; // 要切换到的场景名称
-    private void OnTriggerEnter2D(Collider2D player)
+    void OnCollisionEnter2D(Collision2D collision)
     {
         // 如果玩家则切换场景
         Debug.Log("触发场景切换");
-        if (player.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             // 切换到下一个场景
             Debug.Log("切换到下一个场景");
 
-            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);        }
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Map1");
+        }
     }
 }
